b1d8e52de8f5911cf4bb85b8765b18168a004dfd
Fernando Sena <fernando.sena@mkdata.com.br>
2024-01-30 13:01:07 -0300

    altera db para postgresql em nuvem

--- prisma/migrations/20231208213417_cdd_backend/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `post` DROP FOREIGN KEY `Post_ibfk_1`;
-
--- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_ibfk_1` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
